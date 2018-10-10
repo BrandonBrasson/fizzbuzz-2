512c9ed2c1f7632f4be0f5eb4ba5fd078bef69b2
Brandon Brasson <31114139+BrandonBrasson@users.noreply.github.com>
2018-10-09 20:50:16 -0400

    Add files via upload

---
+++ FizzBuzz/lib/fizzbuzz/test/coc.rb
@@ -0,0 +1,9 @@
+require "fizzbuzz//test//coc/version"
+
+module Fizzbuzz
+  module Test
+    module Coc
+      # Your code goes here...
+    end
+  end
+end
---
+++ FizzBuzz/lib/fizzbuzz/test/coc/version.rb
@@ -0,0 +1,7 @@
+module Fizzbuzz
+  module Test
+    module Coc
+      VERSION = "0.1.0"
+    end
+  end
+end
